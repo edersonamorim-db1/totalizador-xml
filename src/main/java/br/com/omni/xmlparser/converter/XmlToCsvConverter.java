8b5a3cfb530777a3f37df4b987203b7c2b479014
edersonamorim-db1 <ederson.amorim@db1.com.br>
2025-02-21 17:01:05 -0300

    remover impressoes e geracao de arquivo

--- src/main/java/br/com/omni/xmlparser/converter/XmlToCsvConverter.java
+++ src/main/java/br/com/omni/xmlparser/converter/XmlToCsvConverter.java
@@ -1,32 +1,26 @@
 package br.com.omni.xmlparser.converter;
 
-import br.com.omni.xmlparser.domain.RegistroOperacao;
-import br.com.omni.xmlparser.domain.Totalizador;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamReader;
 
 public class XmlToCsvConverter {
 
-    public void convert(String inputXmlPath, String outputCsvPath, int intervaloRelatorio) {
+    private int clientesProcessados = 0;
+    private final Map<String, Map<String, Double>> totais = new HashMap<>();
+
+    public void convert(String inputXmlPath) {
         XMLInputFactory factory = XMLInputFactory.newInstance();
         factory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
         factory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
 
-        Totalizador totalizador = new Totalizador(intervaloRelatorio);
-
-        try (FileInputStream fis = new FileInputStream(inputXmlPath);
-             BufferedWriter writer = new BufferedWriter(new FileWriter(outputCsvPath))) {
-
-            writer.write("DtBase;CNPJ;Cd;Mod;Venc;Valor");
-            writer.newLine();
+        try (FileInputStream fis = new FileInputStream(inputXmlPath)) {
 
             XMLStreamReader reader = factory.createXMLStreamReader(fis);
-            RegistroOperacao registro = null;
-            String clienteAtual = null;
+            String mod = null;
 
             while (reader.hasNext()) {
                 int event = reader.next();
@@ -36,71 +30,51 @@ public class XmlToCsvConverter {
                         String elementName = reader.getLocalName();
 
                         switch (elementName) {
-                            case "Doc3040":
-                                registro = new RegistroOperacao();
-                                registro.setDtBase(reader.getAttributeValue(null, "DtBase"));
-                                registro.setCnpj(reader.getAttributeValue(null, "CNPJ"));
-                                break;
-
-                            case "Cli":
-                                if (registro != null) {
-                                    String novoCli = reader.getAttributeValue(null, "Cd");
-                                    if (!novoCli.equals(clienteAtual)) {
-                                        clienteAtual = novoCli;
-                                        totalizador.incrementarClientes();
-                                    }
-                                    registro.setCdCli(novoCli);
-                                }
-                                break;
-
                             case "Op":
-                                if (registro != null) {
-                                    registro.setMod(reader.getAttributeValue(null, "Mod"));
-                                }
+                                mod = reader.getAttributeValue(null, "Mod");
                                 break;
 
                             case "Venc":
-                                if (registro != null) {
-                                    for (int i = 0; i < reader.getAttributeCount(); i++) {
-                                        String attrName = reader.getAttributeLocalName(i);
-                                        String attrValue = reader.getAttributeValue(i);
-                                        registro.addVencimento(attrName, attrValue);
-                                    }
-
-                                    totalizador.adicionarValores(registro.getMod(), registro.getVencimentos());
-
-                                    for (String  venc : registro.getVencimentosKeys()) {
-                                        writer.write(String.format("%s;%s;%s;%s;%s;%s%n",
-                                                registro.getDtBase(),
-                                                registro.getCnpj(),
-                                                registro.getCdCli(),
-                                                registro.getMod(),
-                                                venc,
-                                                registro.getValor(venc)
-                                        ));
-                                    }
+                                for (int i = 0; i < reader.getAttributeCount(); i++) {
+                                    String attrName = reader.getAttributeLocalName(i);
+                                    String attrValue = reader.getAttributeValue(i);
+                                    adicionarValor(mod, attrName, Double.parseDouble(attrValue));
                                 }
                                 break;
                         }
                         break;
 
                     case XMLStreamConstants.END_ELEMENT:
-                        if ("Doc3040".equals(reader.getLocalName())) {
-                            registro = null;
+                        if ("Cli".equals(reader.getLocalName())) {
+                            clientesProcessados++;
                         }
                         break;
                 }
             }
-
-            // Imprime totais finais
-            System.out.println("\n=== TOTAIS FINAIS ===");
-            totalizador.imprimirTotais();
-
+            imprimirTotais();
             reader.close();
-
         } catch (Exception e) {
             System.err.println("Erro ao processar arquivo: " + e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private void adicionarValor(String mod, String venc, Double valor) {
+        totais.computeIfAbsent(mod, k -> new HashMap<>());
+        totais.get(mod).merge(venc, valor, Double::sum);
+    }
+
+    private void imprimirTotais() {
+        System.out.println("\n=== " + clientesProcessados + " clientes processados ===");
+        totais.keySet().stream().sorted().forEach(
+                mod ->  {
+                    System.out.println("\nModalidade: " + mod);
+                    Map<String, Double> valorPorMod = totais.get(mod);
+                    valorPorMod.keySet().stream().sorted().forEach(
+                            vencimento -> {
+                                System.out.printf("  %s: R$ %,.2f%n", vencimento, valorPorMod.get(vencimento));
+                            });
+                });
+        System.out.println("=======================================\n");
     }
 }
